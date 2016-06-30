5687a183113efcb2c8822beddf26396db2083244
hmc-cs-gdiehl <gdiehl@g.hmc.edu>
2016-06-30 11:16:32 -0400

    Added cache for most recent content and PatchModel

--- parser.py
+++ parser.py
@@ -76,90 +76,106 @@ def history2snap(title, remove=True):
         Converts a Wikipedia page, title, to an edge-list
     """
     if remove:
-        remList=getRemlist("full_histories/"+ title.replace(" ", "_"))
+        cachefile = title.replace(" ", "_") + "_rem.txt"
+    else:
+        cachefile = title.replace(" ", "_") + ".txt"
 
-    # Set up snap file
     if not os.path.isdir('edgelists'):
         os.mkdir('edgelists')
 
-    graphFile = open("edgelists/" + title.replace(" ", "_") + ".txt", "w")
-    graphFile.write("# Directed graph: " + title + ".txt\n")
-    graphFile.write("# Save as tab-separated list of edges\n")
-    graphFile.write("# FromNodeId   ToNodeId\n")
+    if not os.path.isfile("edgelists/" + cachefile):
+        if remove:
+            remList=getRemlist("full_histories/"+ title.replace(" ", "_"))
 
-    print "Applying model . . ."
+        graphFile = open("edgelists/" + cachefile, "w")
+        graphFile.write("# Directed graph: " + title + ".txt\n")
+        graphFile.write("# Save as tab-separated list of edges\n")
+        graphFile.write("# FromNodeId   ToNodeId\n")
 
-    model = PatchModel()
-    prev = []
-    pid=0
+        print "Applying model . . ."
+
+        model = PatchModel()
+        prev = []
+        pid=0
     
-    getid = True # can read id from doc
-    useid= False   # have an id ready to use
-    compare = False  # ready to compare content
-    writeText= False  # adding to current content
+        getid = True # can read id from doc
+        useid= False   # have an id ready to use
+        compare = False  # ready to compare content
+        writeText= False  # adding to current content
     
-    historyFile=open("full_histories/"+ title.replace(" ", "_"), "r")
+        historyFile=open("full_histories/"+ title.replace(" ", "_"), "r")
 
-    line = historyFile.readline().strip()
-    while line[:4] != "<id>":
-        line=historyFile.readline().strip()
+        line = historyFile.readline().strip()
+        while line[:4] != "<id>":
+            line=historyFile.readline().strip()
 
-    for line in historyFile:
-        line=line.strip()
+        for line in historyFile:
+            line=line.strip()
 
 
-        # Gets the next valid revision id
-        if getid:
-            if line[:4] == "<id>":
-                rvid = line[4:-5]
-                if remove and rvid in  remList: 
-                    remList.remove(rvid)
+            # Gets the next valid revision id
+            if getid:
+                if line[:4] == "<id>":
+                    rvid = line[4:-5]
+                    if remove and rvid in  remList: 
+                        remList.remove(rvid)
+                    else:
+                        useid=True
+                        getid=False
+
+            # Have an id ready to use, looking for start of content
+            if useid:
+                if line[:5] == "<text":
+                    content= ""
+                    line = line.split('">')
+                    if len(line) == 1:
+                        line += [""]
+                    line = line[1]+"\n"
+                    useid=False
+                    writeText=True
+        
+            # Have reached start if content, looking for end
+            if writeText:
+                if line[-7:] == "</text>":
+                    content+=line[:-7]
+                    writeText=False
+                    compare = True
                 else:
-                    useid=True
-                    getid=False
-
-        # Have an id ready to use, looking for start of content
-        if useid:
-            if line[:5] == "<text":
-                content= ""
-                line = line.split('">')
-                if len(line) == 1:
-                    line += [""]
-                line = line[1]+"\n"
-                useid=False
-                writeText=True
-        
-        # Have reached start if content, looking for end
-        if writeText:
-            if line[-7:] == "</text>":
-                content+=line[:-7]
-                writeText=False
-                compare = True
-            else:
-                content+=line+"\n"
+                   content+=line+"\n"
 
         
-        # Have text ready to compare. 
-        # Apply to the PatchModel and write dependencies to graph.
-        if compare:
-            contentList=content.split()
-            ps = PatchSet.psdiff(pid, prev, contentList)
+            # Have text ready to compare. 
+            # Apply to the PatchModel and write dependencies to graph.
+            if compare:
+                contentList=content.split()
+                ps = PatchSet.psdiff(pid, prev, contentList)
 
-            pid+=len(ps.patches)
-            for p in ps.patches:
-                depends = model.apply_patch(p) #list of out-edges from rev
-                for d_pid in depends:
-                    graphFile.write( str(p.pid) + "\t" + str(d_pid) + "\n")
+                pid+=len(ps.patches)
+                for p in ps.patches:
+                    depends = model.apply_patch(p) #list of out-edges from rev
+                    for d_pid in depends:
+                        graphFile.write( str(p.pid) + "\t" + str(d_pid) + "\n")
             
-            prev = contentList
-            compare = False
-            getid = True
+                prev = contentList
+                compare = False
+                getid = True
 
         
-    historyFile.close()
-    graphFile.close()
+        historyFile.close()
+        graphFile.close()
     
-    return model.model, content
+        modelFile = open("models/"+ cachefile, "w")
+        line = ""
+        for patch in model.model:
+            line+= str(patch[0])+' '+str(patch[1])+'\n'
+        modelFile.write(line)
+        modelFile.close()
+
+        contentFile = open("content/"+ cachefile, "w")
+        contentFile.write(content)
+        contentFile.close()
+    
+    return 
 
 
 def parse_args():
