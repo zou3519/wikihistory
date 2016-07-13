87c86835cc2fbf993f7e04d9bc2ac548feebe6ac
hmc-cs-gdiehl <gdiehl@g.hmc.edu>
2016-07-13 15:00:56 -0400

    Added height function that measures height from a given start date

--- metric.py
+++ metric.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import networkx as nx
+import timestamp as ts
 import wiki2graph
 
 def getHeight(graph):
@@ -18,9 +19,48 @@ def getHeight(graph):
                 height += (heightDict[intdst] + dist)*prob
             else:
                 prob=graph.edge[src][dst]['prob']
-                height += (heightDict[dst] + dist)*prob 
-    
+                height += (heightDict[dst] + dist)*prob
+
         if type(node)!=int:
             node = int(node.decode("utf-8"))
         heightDict[node]= height
+
     return heightDict
+
+
+def getHeight2(graph, startDate):
+    """
+    """
+    startDate=ts.string2date(startDate)
+    nodeList = nx.topological_sort(graph, reverse = True)
+    heightDict = {}
+    for node in nodeList:
+        height = 0
+        for (src, dst, dist) in graph.out_edges_iter(node, data='dist'):
+            if type(dst) != int:
+                intdst = int(dst.decode("utf-8"))
+                intsrc = int(src.decode("utf-8"))
+                date=graph.node[intsrc]['time']
+                date=ts.ts2date(date)
+                if date<startDate:
+                    height=0
+                else:
+                    prob = graph.edge[src][dst]['prob'] 
+                    height += (heightDict[intdst] + dist)*prob
+            else:
+                date=graph.node[src]['time']
+                date=ts.ts2date(date)
+                if date < startDate:
+                    height=0
+                else:
+                    prob=graph.edge[src][dst]['prob']
+                    height += (heightDict[dst] + dist)*prob
+
+        if type(node)!=int:
+            node = int(node.decode("utf-8"))
+        heightDict[node]= height
+
+    return heightDict
+
+
+
--- timestamp.py
+++ timestamp.py
@@ -7,32 +7,33 @@ def time_diff(oldTime, newTime):
     """
     MINTIME = 0.01
 
-    oldTime=oldTime.split("-")
-    oyear= int(oldTime[0])
-    omonth=int(oldTime[1])
-    odt=oldTime[2].split("T")
-    oday=int(odt[0])
-    odt=odt[1].split(":")
-    ohour=int(odt[0])
-    ominute = int(odt[1])
-    osecond=int(odt[2].strip("Z"))
-
-    newTime=newTime.split("-")
-    nyear= int(newTime[0])
-    nmonth=int(newTime[1])
-    ndt=newTime[2].split("T")
-    nday=int(ndt[0])
-    ndt=ndt[1].split(":")
-    nhour=int(ndt[0])
-    nminute = int(ndt[1])
-    nsecond=int(ndt[2].strip("Z"))
-
-
-    oDate=datetime(oyear, omonth, oday, ohour, ominute, osecond)
-    nDate=datetime(nyear, nmonth, nday, nhour, nminute, nsecond)
-
+    oDate=ts2date(oldTime)
+    nDate=ts2date(newTime)
     delta=nDate-oDate
     minutes= float(delta.total_seconds())/60
     if minutes==0:
         minutes=MINTIME
     return minutes
+
+def ts2date(ts):
+    """
+    """
+    ts=ts.split("-")
+    year= int(ts[0])
+    month=int(ts[1])
+    time=ts[2].split("T")
+    day=int(time[0])
+    time=time[1].split(":")
+    hour=int(time[0])
+    minute = int(time[1])
+    second=int(time[2].strip("Z"))
+
+    return datetime(year, month, day, hour, minute, second)
+
+def string2date(s):
+    """
+        month-day-year
+    """
+    s=s.split('-')
+    return datetime(int(s[2]), int(s[0]), int(s[1]), 0, 0, 0)
+
