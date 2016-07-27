142078d3667e2de4a4b0a39c95197e08b4cc4ca2
hmc-cs-gdiehl <gdiehl@g.hmc.edu>
2016-07-27 14:05:40 -0400

    Removed time from distance calculation

--- metric.py
+++ metric.py
@@ -7,92 +7,6 @@ import wiki2graph as w2g
 import metric2color as m2c
 
 
-def tHeight(graph):
-    """
-    """
-    stime=graph.node[0]['time']
-    etime=graph.node[graph.nodes()[-1]]['time']
-    #T=ts.time_diff(stime, etime)
-
-    nodeList = nx.topological_sort(graph, reverse = True)
-    heightDict = {}
-    
-    for node in nodeList:
-        height = 0
-        for (src, dst, prob) in graph.out_edges_iter(node, data='prob'):
-            if type(dst) != int:
-                dst = int(dst.decode("utf-8"))
-                src = int(src.decode("utf-8")) 
-            dist=graph.edge[src][dst]['dist']
-            height += (heightDict[dst]+dist)*prob
-
-        if type(node)!=int:
-            node = int(node.decode("utf-8"))
-        heightDict[node]= height
-    
-    for node in heightDict:
-        #t=ts.time_diff(graph.node[node]['time'], etime)
-        #if t==0:
-        #    t=1.0/T
-        #heightDict[node] = heightDict[node]/t
-        t = ts.time_diff(stime, graph.node[node]['time'])
-
-        heightDict[node] = heightDict[node]*t
-    
-    return heightDict
-
-
-def allkHeights(graph):
-    """
-        Returns a dictionary of the vertices and their damped, weighted
-            heights from the first vertex
-    """
-    heightDict={}
-    k=0.75
-    nodeList = nx.topological_sort(graph, reverse = True)
-    for node in nodeList:
-        height=0
-        for (src, dst, prob) in graph.out_edges_iter(node, data='prob'):
-            if type(dst) != int:
-                dst = int(dst.decode("utf-8"))
-                src = int(src.decode("utf-8")) 
-            height += (k*heightDict[dst]+graph.edge[src][dst]['dist'])*prob
-        heightDict[node]= height
-    
-    return heightDict
-
-
-
-
-def kHeight(graph, startDate):
-    """
-        Returns a dictionary of the vertices and their damped, weighted heights 
-            from the first vertices at or after startDate.
-    """
-    k=1.0
-    startDate=ts.string2date(startDate)
-    nodeList = nx.topological_sort(graph, reverse = True)
-    heightDict = {}
-    for node in nodeList:
-        height = 0
-        for (src, dst, prob) in graph.out_edges_iter(node, data='prob'):
-            if type(dst) != int:
-                dst = int(dst.decode("utf-8"))
-                src = int(src.decode("utf-8"))
-            date=graph.node[src]['time']
-            date=ts.ts2date(date)
-            if date < startDate:
-                height=0
-            else:
-                height += (k*heightDict[dst]+graph.edge[src][dst]['dist'])*prob 
-
-        if type(node)!=int:
-            node = int(node.decode("utf-8"))
-        heightDict[node]= height
-
-    return heightDict
-
-
 
 
 
@@ -155,20 +69,15 @@ def wiki2color(title, remove, new, allrevs, startDate, shade, metricName):
         Produces a heatmap of the metric height over the most recent revision.
     """
     (graph, content, model) = w2g.wiki2graph(title, remove, new)
-    #if allrevs:
-    #   metricDict=getAllHeights(graph)
-    #else:
-    #    metricDict=getHeight(graph, startDate)
-    #if allrevs:
-    #   metricDict=allkHeights(graph)
-    #else:
-     #   metricDict=kHeight(graph, startDate)
-    metricDict=tHeight(graph)
-    if shade:
-        m2c.metric2shades(title, remove, metricName, metricDict)
+    if allrevs:
+       metricDict=getAllHeights(graph)
     else:
-       # m2c.metric2color(title, remove, metricName, metricDict)
-       m2c.metric2HUSL(title, remove, metricName, metricDict)
+        metricDict=getHeight(graph, startDate)
+   
+    if shade:
+        m2c.metric2HUSL(title, remove, metricName, metricDict)
+    else:
+        m2c.metric2color(title, remove, metricName, metricDict)
 
 
 
